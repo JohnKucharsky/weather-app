d3c60f661eabd148c67385231695ff9b99d9f834
ivan <vannok@live.com>
2025-02-21 22:30:49 +0300

    http to https

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,3 @@
-export const WEATHER_URL = 'http://api.openweathermap.org/data/2.5/forecast'
-export const GEO_URL = 'http://api.openweathermap.org/geo/1.0/direct'
+export const WEATHER_URL = 'https://api.openweathermap.org/data/2.5/forecast'
+export const GEO_URL = 'https://api.openweathermap.org/geo/1.0/direct'
 export const APP_ID = import.meta.env.VITE_OPEN_WEATHER_KEY as string
